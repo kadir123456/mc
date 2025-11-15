b253d7df37b39d69b1bf0152629addad3fc24e77
kadir123456 <47676558+kadir123456@users.noreply.github.com>
2025-11-15 19:01:11 +0300

    Update ImageAnalysis.tsx

--- src/pages/ImageAnalysis.tsx
+++ src/pages/ImageAnalysis.tsx
@@ -245,7 +245,7 @@ export const ImageAnalysis: React.FC = () => {
                     type="button"
                     className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-medium transition"
                   >
-                    Dosya Seç
+                    Boşluğa Tıkla
                   </button>
                 </label>
                 <p className="text-xs text-slate-500 mt-4">
